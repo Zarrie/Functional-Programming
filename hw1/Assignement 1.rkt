151d30c0aa549bd27915c12f861443280f3016e7
Zarrie <22859401+Zarrie@users.noreply.github.com>
2017-10-14 12:16:02 +0300

    Add files via upload

--- hw1/Assignement 1.rkt
+++ hw1/Assignement 1.rkt
@@ -46,26 +46,3 @@
 (define fast-pow (lambda (x n)
                    (if (even n) (* (pow x (/ n 2)) (pow x (/ n 2)))
                        (* x (pow x (- n 1))))))
-;----------------------------------------------------------------------------------------------------------------------------------
-
-;                                             -----------Assignement 1 solutions----------
-
-;                                ---Task 1---
-;convering temperature F -> C                                                                       // task1
-(define f-to-c (lambda (x)
-                  (if (< x -273)
-                      (display "Invalid input argument!"))
-                 (display (* (/ 5.0 9.0) (- x 32.0))) (display " C")))
-
-;                                ---Task 2---
-;function returning true if exactly one number in given list is == 2 else returns false             // task2
-(define three-grades ( lambda (x y z)
-                        (if (= (count (list x y z) 2) 1)
-                            #t
-                            #f)))
-
-;auxiliary function for task 2 which counts number occurances of el in lst
-(define (count lst el)                                 ;Taking as argument list and element
-  (cond ((null? lst) 0)                                ;If list is empty return 0
-        ((= (car lst) el) (+ 1 (count (cdr lst) el)))  ;If list's first element = el return (count(crd list)) + 1
-        (else (count (cdr lst) el))))                  ;If list's first element != el countinue with crd list
