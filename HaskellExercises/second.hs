f6bc22169f3697fcc26526397b0d70129668dde1
Zarrie <22859401+Zarrie@users.noreply.github.com>
2018-01-10 14:06:01 +0200

    Add files via upload

--- HaskellExercises/second.hs
+++ HaskellExercises/second.hs
@@ -48,8 +48,49 @@ difference (x:xs) l = if x `elem` l
 	then difference xs l
 	else x : difference xs l
 
-flip' :: (a->b->c) -> b -> a -> c
-flip' f x y = f y x
+func :: Int -> Int -> Int
+func x y = 2 * x + 3 * y
 
-flip'' :: (a->b->c) -> b -> a -> c
-flip'' f = \x y -> f y x
+flip' :: (x -> y -> z) -> y -> x -> z
+flip' f = \z y -> f y z
+
+flip'' :: (x->y->z) -> y -> x -> z
+flip'' f x y = f y x
+-- evens :: Integral a => [a] -> [a]
+evens l = filter even l
+
+-- evens2 :: [Integer] -> [Integer]
+evens2 = filter even
+
+square l = map (\x -> x * x) l
+
+filterM :: (a -> Bool) -> [[a]] -> [[a]]
+filterM pred matrix = map (\row -> filter pred row) matrix
+
+filterM' :: (a -> Bool) -> [[a]] -> [[a]]
+filterM' pred matrix = map (filter pred) matrix
+
+filterM'' :: (a -> Bool) -> [[a]] -> [[a]]
+filterM'' p = map (filter p)
+
+
+getCol :: [[a]] -> Int -> [a]
+getCol m col = map (\row -> row !! col) m
+
+getCol' :: Int -> [[a]] -> [a]
+getCol' col m = map (!! col) m
+
+diag:: [[a]] -> [a]
+diag m = map (\idx -> (m !! idx) !! idx) [0..(length m - 1)]
+
+secDiag :: [[b]] -> [b]
+secDiag m = map (\idx -> (m !! idx) !! (length m - idx - 1)) [0..(length m - 1)]
+
+transpose :: [[a]] -> [[a]]
+transpose m = map (getCol m) [0..(length m - 1)]
+
+rotate :: [a] -> Int -> [a]
+rotate l 0 = l
+rotate l n
+	| n < 0 || n > length l = rotate l (mod n (length l))
+	| otherwise = rotate (tail l ++ [head l]) (n - 1)
